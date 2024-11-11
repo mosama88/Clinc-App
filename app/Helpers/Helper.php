fdd89681513171ceb1567698eead94d37bd063d6
mosama88 <mosama88@hotmail>
2024-11-11 20:16:55 +0200

    Done Autoload

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+function uploadImage($folder, $image)
+{
+    $extension = strtolower($image->extension());
+    $filename = time() . rand(100, 999) . '.' . $extension;
+    $image->getClientOriginalName = $filename;
+    $image->move($folder, $filename);
+    return $filename;
+}
