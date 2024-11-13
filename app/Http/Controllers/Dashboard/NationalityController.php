2020f6582a4aa69b72c3e48cc99f85d8a5085dd1
mosama88 <mosama88@hotmail>
2024-11-13 15:58:32 +0200

    Done Good2

--- app/Http/Controllers/Dashboard/NationalityController.php
+++ app/Http/Controllers/Dashboard/NationalityController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Models\Nationality;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\NationalityRequest;
+use App\Http\Requests\Dashboard\NationalityUpdateRequest;
 
 class NationalityController extends Controller
 {
-    /**
+   /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $com_code = auth()->user()->com_code;
+        $data = Nationality::select("*")->where('com_code',$com_code)->orderBy('id','DESC')->get();
+        return view('dashboard.nationalities.index',compact('data'));
     }
 
     /**
@@ -20,15 +26,29 @@ class NationalityController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.nationalities.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(NationalityRequest $request)
     {
-        //
+        try{
+            $com_code = auth()->user()->com_code;
+            DB::beginTransaction();
+           $blood = new Nationality();
+           $blood['name'] = $request->name;
+           $blood['created_by'] = 1;
+           $blood['com_code'] = $com_code;
+           $blood->save();
+            DB::commit();
+            return redirect()->route('dashboard.nationalities.index')->with('success', 'تم أضافة الجنسية بنجاح');            
+            
+        }catch(\Exeption $ex){
+            DB::rollback();
+            return redirect()->route('dashboard.nationalities.index')->withErrors('error', 'عفوآ لقد حدث خطأ !!' . $ex->getMessage());
+        }
     }
 
     /**
@@ -44,15 +64,31 @@ class NationalityController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $info = Nationality::findOrFail($id);
+        return view('dashboard.nationalities.edit',compact('info'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(NationalityUpdateRequest $request, string $id)
     {
-        //
+        try{
+            $com_code = auth()->user()->com_code;
+            DB::beginTransaction();
+           $Updateblood = Nationality::findOrFail($id);
+           $Updateblood['name'] = $request->name;
+           $Updateblood['updated_by'] = 1;
+           $Updateblood['com_code'] = $com_code;
+           $Updateblood->save();
+            DB::commit();
+            return redirect()->route('dashboard.nationalities.index')->with('success', 'تم تعديل الجنسية بنجاح');            
+            
+        }catch(\Exeption $ex){
+            DB::rollback();
+            return redirect()->route('dashboard.nationalities.index')->withErrors('error', 'عفوآ لقد حدث خطأ !!' . $ex->getMessage());
+        }
     }
 
     /**
@@ -60,6 +96,18 @@ class NationalityController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try{
+            $com_code = auth()->user()->com_code;
+            DB::beginTransaction();
+           $Deleteblood = Nationality::findOrFail($id);
+           $Deleteblood->delete();
+            DB::commit();
+            return redirect()->route('dashboard.nationalities.index')->with('success', 'تم حذف الجنسية بنجاح');            
+            
+        }catch(\Exeption $ex){
+            DB::rollback();
+            return redirect()->route('dashboard.nationalities.index')->withErrors('error', 'عفوآ لقد حدث خطأ !!' . $ex->getMessage());
+        }
+
     }
 }
--- resources/views/dashboard/nationalities/index.blade.php
+++ resources/views/dashboard/nationalities/index.blade.php
@@ -1,10 +1,10 @@
 @extends('dashboard.layouts.master')
-@section('admin_title', 'العملات')
+@section('admin_title', 'فصيلة الدم')
 @section('css')
 @endsection
 @section('active-currency', 'active')
-@section('page-header', 'جدول العملات')
-@section('page-header_desc', 'جدول العملات')
+@section('page-header', 'جدول فصيلة الدم')
+@section('page-header_desc', 'جدول فصيلة الدم')
 @section('page-header_link')
     <li class="breadcrumb-item"><a href="{{ url('/') }}">لوحة التحكم</a></li>
 @endsection
@@ -22,11 +22,14 @@
             {{-- Content --}}
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">جدول العملات</h3>
+                    <h3 class="card-title">جدول فصيلة الدم</h3>
                 </div>
                 <div class="card-header">
-                    <a type="button" href="{{ route('dashboard.currencies.create') }}"
-                        class="btn btn-md btn-primary btn-flat"><i class="fas fa-plus ml-2"></i> أضافة عملة جديده</a>
+                    <button type="button" class="btn btn-md btn-primary btn-flat" data-toggle="modal"
+                        data-target="#modal-default">
+                        <i class="fas fa-plus ml-2"></i> أضافة فصيلة جديده
+                    </button>
+                    @include('dashboard.BloodTypes.create')
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body p-0">
@@ -34,9 +37,9 @@
                         <thead>
                             <tr>
                                 <th style="width: 10px">#</th>
-                                <th>أسم العملة</th>
-                                <th>قيمة العملة</th>
-                                <th>وصف العملة</th>
+                                <th>فصيلة الدم</th>
+                                <th>أضافة بواسطة</th>
+                                <th>تعديل بواسطة</th>
                                 <th>العمليات</th>
                             </tr>
                         </thead>
@@ -47,8 +50,14 @@
                                 <tr>
                                     <td>{{ $i }}</td>
                                     <td>{{ $info['name'] }}</td>
-                                    <td>{{ $info['description'] }}</td>
-                                    <td>{{ $info['amount'] }}</td>
+                                    <td>{{ $info->createdBy->name }}</td>
+                                    <td>
+                                        @if ($info->updated_by > 0)
+                                            {{ $info->updatedBy->name }}
+                                        @else
+                                            لا يوجد تحديث
+                                        @endif
+                                    </td>
                                     <td>
                                         <div class="btn-group">
                                             <button type="button" class="btn btn-info">العلميات</button>
@@ -59,16 +68,26 @@
                                             </button>
                                             <div class="dropdown-menu" role="menu" x-placement="top-start"
                                                 style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-1px, -165px, 0px);">
-                                                <a class="dropdown-item"
-                                                    href="{{ route('dashboard.currencies.edit', $info['id']) }}"><i
-                                                        class="fas fa-edit ml-2"></i>
-                                                    تعديل</a>
-                                                <a class="dropdown-item modal-effect btn btn-sm" data-effect="effect-scale"
-                                                    data-toggle="modal" href="#delete{{ $info->id }}">
-                                                    <i class="fas fa-trash-alt ml-1"></i>حذف</a>
+
+
+                                                <button class="dropdown-item" type="button"
+                                                    class="btn btn-md btn-primary btn-flat" data-toggle="modal"
+                                                    data-target="#edit{{ $info->id }}">
+                                                    <i class="fas fa-edit ml-2"></i>
+                                                    تعديل
+                                                </button>
+
+
+                                                <button class="dropdown-item" type="button"
+                                                    class="btn btn-md btn-primary btn-flat" data-toggle="modal"
+                                                    data-target="#delete{{ $info->id }}">
+                                                    <i class="fas fa-trash-alt ml-1"></i>حذف
+                                                </button>
+
                                             </div>
                                         </div>
-                                        @include('dashboard.currencies.delete')
+                                        @include('dashboard.BloodTypes.delete')
+                                        @include('dashboard.BloodTypes.edit')
                                     </td>
 
                                 </tr>
@@ -88,4 +107,29 @@
 
 @endsection
 @section('scripts')
+
+    <script>
+        $('#modal-default').modal({
+            keyboard: false,
+            show: false,
+            backdrop: 'static',
+
+        });
+
+        $('.Edit-modal-default').modal({
+            keyboard: false,
+            show: false,
+            backdrop: 'static',
+
+        });
+
+        $('.Delete-modal-default').modal({
+            keyboard: false,
+            show: false,
+            backdrop: 'static',
+
+        });
+    </script>
+
+
 @endsection
