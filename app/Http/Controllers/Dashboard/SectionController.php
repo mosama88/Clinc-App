b426f5afc1a0010d411ad0ef18dd99e99c8239b9
mosama88 <mosama88@hotmail>
2024-11-13 16:12:09 +0200

    Done Good4

--- app/Http/Controllers/Dashboard/SectionController.php
+++ app/Http/Controllers/Dashboard/SectionController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Models\Section;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\SectionRequest;
+use App\Http\Requests\Dashboard\SectionUpdateRequest;
 
 class SectionController extends Controller
 {
-    /**
+     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $com_code = auth()->user()->com_code;
+        $data = Section::select("*")->where('com_code',$com_code)->orderBy('id','DESC')->get();
+        return view('dashboard.sections.index',compact('data'));
     }
 
     /**
@@ -20,15 +26,29 @@ class SectionController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.sections.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SectionRequest $request)
     {
-        //
+        try{
+            $com_code = auth()->user()->com_code;
+            DB::beginTransaction();
+           $blood = new Section();
+           $blood['name'] = $request->name;
+           $blood['created_by'] = 1;
+           $blood['com_code'] = $com_code;
+           $blood->save();
+            DB::commit();
+            return redirect()->route('dashboard.sections.index')->with('success', 'تم أضافة القسم بنجاح');            
+            
+        }catch(\Exeption $ex){
+            DB::rollback();
+            return redirect()->route('dashboard.sections.index')->withErrors('error', 'عفوآ لقد حدث خطأ !!' . $ex->getMessage());
+        }
     }
 
     /**
@@ -44,15 +64,31 @@ class SectionController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $info = Section::findOrFail($id);
+        return view('dashboard.sections.edit',compact('info'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(SectionUpdateRequest $request, string $id)
     {
-        //
+        try{
+            $com_code = auth()->user()->com_code;
+            DB::beginTransaction();
+           $Updateblood = Section::findOrFail($id);
+           $Updateblood['name'] = $request->name;
+           $Updateblood['updated_by'] = 1;
+           $Updateblood['com_code'] = $com_code;
+           $Updateblood->save();
+            DB::commit();
+            return redirect()->route('dashboard.sections.index')->with('success', 'تم تعديل القسم بنجاح');            
+            
+        }catch(\Exeption $ex){
+            DB::rollback();
+            return redirect()->route('dashboard.sections.index')->withErrors('error', 'عفوآ لقد حدث خطأ !!' . $ex->getMessage());
+        }
     }
 
     /**
@@ -60,6 +96,18 @@ class SectionController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try{
+            $com_code = auth()->user()->com_code;
+            DB::beginTransaction();
+           $Deleteblood = Section::findOrFail($id);
+           $Deleteblood->delete();
+            DB::commit();
+            return redirect()->route('dashboard.sections.index')->with('success', 'تم حذف القسم بنجاح');            
+            
+        }catch(\Exeption $ex){
+            DB::rollback();
+            return redirect()->route('dashboard.sections.index')->withErrors('error', 'عفوآ لقد حدث خطأ !!' . $ex->getMessage());
+        }
+
     }
 }
